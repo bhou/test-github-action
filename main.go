b8a4f1bcb8ed554ecd2b53707c8fc44ca94e0701
bhou <daily.bhou@gmail.com>
2022-04-10 22:44:08 +0200

    Fix build command in action

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+	"text/template"
+)
+
+type Context struct {
+	Os   string
+	Arch string
+	Exe  string
+}
 
 func main() {
 	fmt.Println("Hello World")
+
+	ctx := Context{
+		Os:   "windows",
+		Arch: "arm",
+		Exe:  "",
+	}
+
+	t, err := template.New("test").Parse("{{.Os}}/{{.Arch}}/src/test{{if eq .Os \"windows\"}}.bat{{else if eq .Os \"linux\"}}.sh{{end}}")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	builder := strings.Builder{}
+	err = t.Execute(&builder, ctx)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	fmt.Println(builder.String())
 }
